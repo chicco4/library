28237200ac8b2579e0a1620976b6e3a1125c0181
chicco4 <chiccotto4@gmail.com>
2022-07-03 11:01:54 +0200

    not beautiful but good enough

--- script.js
+++ script.js
@@ -47,13 +47,126 @@ const modal = document.getElementById("modal")
 const addBookForm = document.getElementById("add-book-form")
 const booksGrid = document.getElementById("books-grid")
 
-////////////////////////////////////////////////////////////
+const createBookCard = (book) => {
+    const bookCard = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const buttonGroup = document.createElement('div')
+    const readBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
 
-function addBook() {
+    bookCard.classList.add('book-card')
+    buttonGroup.classList.add('btn-group')
+    readBtn.classList.add('read-btn')
+    removeBtn.classList.add('remove-btn')
+    readBtn.onclick = toggleRead
+    removeBtn.onclick = removeBook
+
+    title.textContent = `${book.title}`
+    author.textContent = book.author
+    pages.textContent = `${book.pages} pages`
+    removeBtn.textContent = 'Remove'
+
+    if (book.isRead) {
+        readBtn.textContent = 'Read'
+        readBtn.classList.add('green')
+    } else {
+        readBtn.textContent = 'Not read'
+        readBtn.classList.add('red')
+    }
+
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    buttonGroup.appendChild(readBtn)
+    buttonGroup.appendChild(removeBtn)
+    bookCard.appendChild(buttonGroup)
+    booksGrid.appendChild(bookCard)
+}
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('isRead').checked
+    return new Book(title, author, pages, isRead)
+}
+
+const addBook = (e) => {
+    e.preventDefault()
+    const newBook = getBookFromInput()
+
+    if (library.isInLibrary(newBook)) {
+        console.log("This book already exists in your library")
+        return
+    }
+
+    library.addBook(newBook)
+    saveLocal()
+    updateBooksGrid()
+
+    closeAddBookModal()
+}
+
+const removeBook = (e) => {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    )
+
+    library.removeBook(title)
+    saveLocal()
+    updateBooksGrid()
 
 }
 
-////////////////////////////////////////////////////
+const toggleRead = (e) => {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    )
+    const book = library.getBook(title)
+
+    book.isRead = !book.isRead
+    saveLocal()
+    updateBooksGrid()
+
+}
+
+// Local Storage
+
+const saveLocal = () => {
+    localStorage.setItem('library', JSON.stringify(library.books))
+}
+
+const restoreLocal = () => {
+    const books = JSON.parse(localStorage.getItem('library'))
+    if (books) {
+        library.books = books.map((book) => JSONToBook(book))
+    } else {
+        library.books = []
+    }
+}
+
+// Utils
+
+const docsToBooks = (docs) => {
+    return docs.map((doc) => {
+        return new Book(
+            doc.data().title,
+            doc.data().author,
+            doc.data().pages,
+            doc.data().isRead
+        )
+    })
+}
+
+const JSONToBook = (book) => {
+    return new Book(book.title, book.author, book.pages, book.isRead)
+}
+
+//BASICS
 
 const updateBooksGrid = () => {
     resetBooksGrid()
@@ -78,6 +191,13 @@ const openModal = () => {
 function closeAddBookModal() {
     modal.classList.remove("active");
 }
+
+function gigio() {
+    restoreLocal()
+    updateBooksGrid()
+}
+
+window.onload = gigio;
 
 addBookBtn.onclick = openModal
 addBookForm.onsubmit = addBook
